52e5d74ca31475b6a1853c5f8a57630fd8175ed0
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2024-07-13 11:10:08 -0300

    Cleaning index.js

--- Drawback.docs/backend/src/index/index.js
+++ Drawback.docs/backend/src/index/index.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let totalAmount = 1;
-    let progress = 8;
-    let number = document.getElementById("number");
-    var amount = document.querySelectorAll(".amountSquares");
-    var size = (472 / progress);
-    for (var counter = 0; counter < totalAmount; counter++);
+
+    let totalAmount, progress, number, amount, size, counter; 
+
+    totalAmount = 1;
+    progress = 8;
+
+    number = document.getElementById("number");
+    amount = document.querySelectorAll(".amountSquares");
+
+    size = (472 / progress);
+
+    for (counter = 0; counter < totalAmount; counter++);
+    
     number.innerHTML = counter + "/" + progress;
     document.body.style.setProperty('--size', 472 - size * totalAmount);
 });
