c587df56cacd6089d3b2b71309af58fc46b3d0d4
Lobo144 <151693524+Lobo144@users.noreply.github.com>
2024-07-31 16:30:04 -0300

    Changes index/js

--- Drawback.docs/src/scripts/index/index.js
+++ Drawback.docs/src/scripts/index/index.js
@@ -18,4 +18,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
+function showModal(){
+    var element = document.getElementById("modal");
+    element.classList.add("show-modal");
+}
+
+function hideModal(){
+    var element = document.getElementById("modal"); 
+    element.classList.remove("show-modal");
+}
+
+
+
 
