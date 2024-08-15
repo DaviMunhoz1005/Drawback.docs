33b14533b2ada836a33003a8584cb92a73394c8d
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2024-08-15 05:36:56 -0300

    Fixing directory from index.js

--- Drawback.docs/src/scripts/index/index.js
+++ Drawback.docs/src/scripts/index/index.js
@@ -1,17 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
+let totalAmount, progress, number, amount, size, counter; 
 
-    let totalAmount, progress, number, amount, size, counter; 
+totalAmount = 1;
+progress = 7;
 
-    totalAmount = 1;
-    progress = 7;
+number = document.getElementById("number");
+amount = document.querySelectorAll(".amountSquares");
 
-    number = document.getElementById("number");
-    amount = document.querySelectorAll(".amountSquares");
+size = (472 / progress);
 
-    size = (472 / progress);
-
-    for (counter = 0; counter < totalAmount; counter++);
+for (counter = 0; counter < totalAmount; counter++);
     
-    number.innerHTML = counter + "/" + progress;
-    document.body.style.setProperty('--size', 472 - size * totalAmount);
-});
+number.innerHTML = counter + "/" + progress;
+document.body.style.setProperty('--size', 472 - size * totalAmount);
