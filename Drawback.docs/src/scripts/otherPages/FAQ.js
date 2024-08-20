4e7a767f26364a6f4a3a0e801a6a1076ce9061d1
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2024-08-20 09:23:16 -0300

    Creating FAQ.js

---
+++ Drawback.docs/src/scripts/otherPages/FAQ.js
@@ -0,0 +1,14 @@
+let x = 0;
+
+window.hideSelectionsForms = function() {
+    document.getElementById('modalentrar').style.display = 'none';
+    x = 0;
+};
+
+window.showSelectionsForms = function() {
+    document.getElementById('modalentrar').style.display = 'block';
+    x++;
+    if (x >= 2) {
+        window.hideSelectionsForms(); 
+    }
+};
